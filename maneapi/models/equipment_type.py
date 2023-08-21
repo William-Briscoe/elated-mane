a90a805ca5995dea228723f84ec12a58453628a2
William Briscoe <will.m.briscoe@gmail.com>
2023-08-21 15:47:44 -0500

    final commit

--- maneapi/models/equipment_type.py
+++ maneapi/models/equipment_type.py
@@ -2,6 +2,9 @@
 from django.db import models
 
 
+
+
 class EquipmentType(models.Model):
     """Equipment type model class"""
     label = models.CharField(max_length=50)
+
--- maneapi/models/style.py
+++ maneapi/models/style.py
@@ -1,6 +1,6 @@
 """Hair styles class module"""
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class HairStyle(models.Model):
     """Hair style model class"""
